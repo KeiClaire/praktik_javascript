9db1181f3767b7ab4c17f621699ea57c66f63324
KeiClaire <herysuparjan550@gmail.com>
2024-01-22 13:29:40 +0800

    praktik 6

--- praktik6.js
+++ praktik6.js
@@ -7,12 +7,28 @@
 // let num = ++str; // unary operator
 // console.log(num)
 
-// contoh 3
-const birthYear = prompt("Tahun Berapa Kamu Lahir?");
-const yourAge = 2024 - Number(birthYear);
-console.log(yourAge);
+// // contoh 3
+// const birthYear = prompt("Tahun Berapa Kamu Lahir?");
+// const yourAge = 2024 - Number(birthYear);
+// console.log(yourAge);
 
-// contoh 4
-let num = 42;
-let str = String(num);
-console.log(str);
+// // contoh 4
+// let num = 42;
+// let str = String(num);
+// console.log(str);
+
+const age = 20;
+const height = 167;
+const weight = 60;
+const isStudent = true;
+
+const ageAsString = String(age);
+const heightAsString = height + "cm";
+const weightAsString = weight + "kg";
+const studentStatus = Number(isStudent);
+
+console.log(typeof ageAsString);
+console.log(typeof heightAsString);
+console.log(typeof weightAsString);
+console.log(typeof studentStatus);
+
